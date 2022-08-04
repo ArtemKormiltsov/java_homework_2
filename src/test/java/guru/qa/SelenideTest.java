a9c373de1aab66d1dc6e6515536d6f76b741a640
Artem Kormiltsov <akormilcov@rvision.pro>
2022-08-04 18:29:46 +0300

    add gitHubTest

---
+++ src/test/java/guru/qa/SelenideTest.java
@@ -0,0 +1,36 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.browserSize = "1920x1020";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void gitHubTest(){
+        open ("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $(byText("SoftAssertions")).click();
+        $("#wiki-content").shouldHave(text("Using JUnit4 just describe rule inside test class:"));
+    }
+
+
+
+
+
+
+
+
+}
